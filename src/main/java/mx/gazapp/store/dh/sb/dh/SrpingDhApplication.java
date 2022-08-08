82661e4ae086f17318005011b62fceb93e77b89d
GazApp <juangaz@hotmail.com>
2022-08-08 09:52:45 -0500

    Initial commit

---
+++ src/main/java/mx/gazapp/store/dh/sb/dh/SrpingDhApplication.java
@@ -0,0 +1,13 @@
+package mx.gazapp.store.dh.sb.dh;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SrpingDhApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SrpingDhApplication.class, args);
+    }
+
+}
---
+++ src/test/java/mx/gazapp/store/dh/sb/dh/SrpingDhApplicationTests.java
@@ -0,0 +1,13 @@
+package mx.gazapp.store.dh.sb.dh;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SrpingDhApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
